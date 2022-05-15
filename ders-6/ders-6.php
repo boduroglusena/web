620e1a97501f500df7358e6099375cb119263571
Senanur <boduroglusena@gmail.com>
2022-05-15 19:11:50 +0300

    Ders-6 tamamlandı

---
+++ ders-6/ders-6.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=<form">
+ 
+    <title>ders-6</title>
+</head>
+<body>
+<div class="form-row">
+  <br>
+    <div class="form-group col-md-6">
+      <label for="inputEmail4">Email:</label>
+      <input type="email" class="form-control" id="inputEmail4" placeholder="">
+    </div>
+    <br>
+    <div class="form-group col-md-6">
+      <label for="inputPassword4">Şifre:</label>
+      <input type="password" class="form-control" id="inputPassword4" placeholder="">
+    </div>
+  </div>
+  <br>
+  <div class="form-group">
+    <label for="inputAddress">Adres:</label>
+    <input type="text" class="form-control" id="inputAddress" placeholder="">
+  </div>
+ <br>
+ <div class="form-group">
+    <label for="inputAddress">Telefon:</label>
+    <input type="text" class="form-control" id="inputAddress" placeholder="">
+  </div>
+ <br>
+  <div class="form-row">
+    <div class="form-group col-md-6">
+      <label for="inputCity">Cinsiyetiniz:</label>
+      <input type="text" class="form-control" id="inputCity">
+    </div>
+    <br>
+    <div class="form-group col-md-4">
+      <label for="inputState">Ders</label>
+      <select id="inputState" class="form-control">
+      <option selected>Ders Seçimi  Yapınız</option>
+        <option >Yazılım mimarileri</option>
+        <option >wep proglamlama</option>
+        <option >sunucu işletim sistemleri</option>
+        
+      </select>
+    </div>
+    <br>
+  </div>
+  <div class="form-group">
+    <div class="form-check">
+      <input class="form-check-input" type="checkbox" id="gridCheck">
+      <label class="form-check-label" for="gridCheck">
+        Beni Hatırla
+      </label>
+    </div>
+  </div>
+  <br>
+  <button type="submit" class="btn btn-primary">Giriş Yap</button>
+</form>
+</body>
+</html>
